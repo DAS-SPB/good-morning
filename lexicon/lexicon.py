20f273b998a7d666b0a020044269db8e7326aea5
AntonioMont <garsiamarkes111@gmail.com>
2024-09-02 17:24:28 +0200

    02.09

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,6 +1,6 @@
 LEXICON_RU: dict[str, str] = {
-    "/start": "Привет!\n\nЯ бот, который будет иногда слать тебе сообщения!\n\n"
-              "Если хочешь, можешь мне что-нибудь написать",
+    "/start": "Привет!\n\nЯ бот, который будет по утрам слать тебе сообщения!\n\n"
+              "Если хочешь, можешь мне что-нибудь написать в ответ",
     "/help": "Попробуй прислать фото или стикер. Или просто отправь текст",
     "unknown_user": "This bot is made exactly for one person. So, your messages will not be proceeded.\n"
                     "Take care of yourself and good luck",
